429cd4001ac5ece206914edf0513c199e6ebe324
Glitch (hello-webpage) <none>
2020-11-18 21:13:25 +0000

    🍲🕋 Checkpoint ./script.js:22513003/649

--- script.js
+++ script.js
@@ -2,8 +2,31 @@
     to your site with Javascript */
 
 // prints "hi" in the browser's dev tools console
+const form = document.querySelector('form');
 const email = document.getElementById('email');
+const emailError = document.querySelector('#email + span.error');
 
 email.addEventListener('input', function(e) {
-  
-})
+  if (email.validity.valid) {
+    emailError.innerHTML = '';
+    emailError.className = 'error';
+  } else {
+    showError();
+  }
+});
+
+form.addEventListener('submit', function(e) {
+  if(!email.validity.valid) {
+    showError();
+    e.preventDefault();
+  }
+});
+
+function showError() {
+  if (email.validity.valueMissing) {
+    emailError.textContent = `Please enter an email address.`;
+  } else if (email.validity.typeMismatch) {
+    emailError.textContent = 'You need to enter a valid email';
+  } else if ()
+}
+
