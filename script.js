ed20a8dd252a89429deb8055391672f6b80782b8
Glitch (hello-webpage) <none>
2020-11-19 23:17:22 +0000

    🦂🌑 Checkpoint ./script.js:22513003/801

--- script.js
+++ script.js
@@ -18,5 +18,38 @@ function deactivateSelect(select) {
   
   let optList = select.querySelector('.optList');
   
-  optList.classList.add
+  optList.classList.add('hidden');
+  
+  select.classList.remove('active');
 }
+
+function activeSelect(select, selectList) {
+  if (select.classList.contains('active')) return;
+  
+  selectList.forEach(deactivateSelect);
+  
+  select.classList.add('active');
+}
+
+function toggleOptList(select) {
+  let optList = select.querySelector('.optList');
+  optList.classList.toggle('hidden');
+}
+
+function highlightOption(select, option) {
+  let optionList = select.querySelector('.option');
+  
+  optionList.forEach(function(other) {
+    other.classList.remove('highlight');
+  });
+  
+  option.classList.add('highlight');
+};
+
+window.addEventListener('load', function() {
+  let selectList = document.querySelectorAll('.select');
+  
+  selectList.forEach(function(select) {
+    let optionList = 
+  })
+})
