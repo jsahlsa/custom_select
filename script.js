dda23858cfaaac604a20045992e2424c3fe0d828
Glitch (hello-webpage) <none>
2020-11-19 20:39:48 +0000

    ⛄️🌕 Checkpoint ./index.html:22513003/4547 ./script.js:22513003/950 ./style.css:22513003/1128

--- script.js
+++ script.js
@@ -2,36 +2,3 @@
     to your site with Javascript */
 
 // prints "hi" in the browser's dev tools console
-const form = document.querySelector('form');
-const email = document.getElementById('email');
-const emailError = document.querySelector('#email + span');
-
-email.addEventListener('input', function(e) {
-  if (email.validity.valid) {
-    emailError.innerHTML = '';
-    emailError.className = 'error';
-  } else {
-    showError();
-  }
-});
-
-form.addEventListener('submit', function(e) {
-  if(!email.validity.valid) {
-    showError();
-    e.preventDefault();
-  }
-});
-
-function showError() {
-  if (email.validity.valueMissing) {
-    emailError.textContent = `Please enter an email address.`;
-  } else if (email.validity.typeMismatch) {
-    emailError.textContent = 'You need to enter a valid email';
-  } else if (email.validity.tooShort) {
-    emailError.textContent = `Email should be at least ${ email.minLength } characters, you entered ${ email.value.length }.`;
-  }
-  
-  emailError.className = 'error active';
-  email.removeAttribute("required", '');
-}
-
