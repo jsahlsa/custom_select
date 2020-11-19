5135fe6bd734ebfd8d4038744f3536469995634c
Glitch (hello-webpage) <none>
2020-11-19 21:14:13 +0000

    ✨🌯 Checkpoint ./index.html:22513003/3378 ./script.js:22513003/233

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
     to your site with Javascript */
 
 // prints "hi" in the browser's dev tools console
+window.addEventListener('load', function() {
+  let form = document.querySelector('form');
+  
+  form.classList.remove('no-widget');
+  form.classList.add('widget');
+})
